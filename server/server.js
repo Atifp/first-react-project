1e9e42aead2c1f27e57a074135e70d3814733e03
Atif Patel <atif.patel@matillion.com>
2023-03-03 13:59:18 +0000

    Backend data successfully passed to front end and displayed

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req,res) => {
-    res.json({"users": ["userOne","userTwo","userThree"]
-    })
+    res.json(
+        {
+            "users": ["userOne","userTwo","userThree"],
+            "shopping": ["chicken", "eggs"]
+        }
+    )
 })
 
 app.listen(5000, () => {console.log("Server has started on port 5000")})
